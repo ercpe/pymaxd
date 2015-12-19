c193ad3d91a6ca34f216de18e12c81a5a1b98a79
Johann Schmitz <johann@j-schmitz.net>
2015-12-19 13:24:29 +0100

    Added more code and tests for config

--- src/maxd/config.py
+++ src/maxd/config.py
@@ -1,18 +1,44 @@
 # -*- coding: utf-8 -*-
+import collections
+import logging
 
 try:
 	from ConfigParser import ConfigParser
 except ImportError: # pragma: no cover
 	from configparser import ConfigParser
 
+logger = logging.getLogger(__name__)
+
+CalendarConfig = collections.namedtuple('CalendarConfig', ('name', 'url'))
+
 class Configuration(object):
 
 	def __init__(self, config_path):
 		self.path = config_path
 		self.cfg_parser = None
+		self._calendar = None
 		self.reload()
 
 	def reload(self):
 		self.cfg_parser = ConfigParser()
 		if not self.cfg_parser.read(self.path) == [self.path]:
 			raise Exception("Failed to read configuration file %s" % self.path)
+
+	def get_option(self, section, option, default=None):
+		return self.cfg_parser.get(section, option) if self.cfg_parser.has_option(section, option) else default
+
+	@property
+	def calendar(self):
+		if self._calendar is None:
+			self._calendar = []
+
+			names = [x.strip() for x in self.get_option('GENERAL', 'calendars', '').split(',') if x.strip()]
+			for section_name in names:
+				url = self.get_option(section_name, 'url')
+				if not url:
+					logger.warning("Ignoring calendar '%s' (missing url)" % section_name)
+					continue
+
+				self._calendar.append(CalendarConfig(section_name, url))
+
+		return self._calendar
--- tests/test_config.py
+++ tests/test_config.py
@@ -15,8 +15,26 @@ class TestConfig(object):
 		cfg.reload()
 
 		assert old_id != id(cfg.cfg_parser), "Configuration did not reload"
+		assert cfg._calendar is None, "Calendar property not cleared on reload"
+
+	def test_calendar_property_init(self):
+		cfg = Configuration('tests/fixtures/config/basic.cfg')
+
+		cals = cfg.calendar
+		assert cals == []  # first access => initialize
+		assert id(cals) == id(cfg.calendar)  # second access => cached value
+
+	def test_calendar_property_missing_values(self):
+		cfg = Configuration('tests/fixtures/config/missing_values.cfg')
+		assert len(cfg.calendar) == 0  # missing url
+
+	def test_calendar_property(self):
+		cfg = Configuration('tests/fixtures/config/basic2.cfg')
+		assert len(cfg.calendar) == 1
+		cal = cfg.calendar[0]
+		assert cal.name == 'testcal1'
+		assert cal.url == 'http://localhost/test.ics'
 
 	def test_basic_config(self):
 		cfg = Configuration('tests/fixtures/config/basic.cfg')
 		assert cfg.cfg_parser is not None
-
