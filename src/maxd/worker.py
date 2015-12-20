00e3e0eeb88943489187edc31b44f1ef292fc366
Johann Schmitz <johann@j-schmitz.net>
2015-12-20 10:08:11 +0100

    Implemented stub for loading calendar events for local files

--- src/maxd/worker.py
+++ src/maxd/worker.py
@@ -1,7 +1,59 @@
 # -*- coding: utf-8 -*-
 import logging
+import collections
+import time
+import datetime
+
+import pytz
+from icalendar import Calendar
+
+try:
+	from urlparse import urlsplit
+except ImportError: # pragma: nocover
+	from urllib.parse import urlsplit
 
 logger = logging.getLogger(__name__)
+
+def _to_utc_datetime(dt):
+	if dt is None:
+		return None
+
+	if dt.tzinfo == pytz.UTC:
+		return dt
+	return datetime.datetime.fromtimestamp(time.mktime(dt.timetuple()), tz=pytz.UTC)
+
+
+class Event(collections.namedtuple('Event', ('name', 'start', 'end'))):
+
+	def __new__(cls, **kwargs):
+		kwargs['start'] = _to_utc_datetime(kwargs.pop('start', None))
+		kwargs['end'] = _to_utc_datetime(kwargs.pop('end', None))
+
+		return super(Event, cls).__new__(cls, **kwargs)
+
+
+class EventFetcher(object):
+
+	def fetch(self, calendar_config):
+		raise NotImplementedError  # pragma: nocover
+
+
+class LocalCalendarEventFetcher(EventFetcher):
+
+	def fetch(self, calendar_config):
+		with open(calendar_config.url, 'r') as f:
+			calendar = Calendar.from_ical(f.read())
+			for item in calendar.walk():
+				if item.name != "VEVENT":
+					continue
+
+				name = item.get('SUMMARY')
+				yield Event(name=name, start=item.get('DTSTART').dt, end=item.get('DTEND').dt)
+
+
+class HTTPCalendarEventFetcher(EventFetcher):
+	pass
+
 
 class Worker(object):
 
@@ -14,8 +66,16 @@ class Worker(object):
 
 		events = []
 		for calendar_config in self.config.calendars:
-			events.extend(self.fetch_events(calendar_config))
+			try:
+				events.extend(self.fetch_events(calendar_config))
+			except:
+				logger.exception("Failed to read events from %s" % calendar_config.name)
 
 	def fetch_events(self, calendar_config):
-		return []
+		chunks = urlsplit(calendar_config.url)
+		if chunks.scheme and chunks.netloc:
+			return HTTPCalendarEventFetcher().fetch(calendar_config)
+		else:
+			return LocalCalendarEventFetcher().fetch(calendar_config)
+
 
--- tests/test_worker.py
+++ tests/test_worker.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
-from maxd.config import Configuration
-from maxd.worker import Worker
+import datetime
+
+import pytz
+from maxd.config import Configuration, CalendarConfig
+from maxd.worker import Worker, LocalCalendarEventFetcher, _to_utc_datetime
 
 
 class TestWorker(object):
@@ -9,3 +12,36 @@ class TestWorker(object):
 		config = Configuration('tests/fixtures/config/basic.cfg')
 		w = Worker(config)
 		w.execute()
+
+	# def test_parse_local_calendar(self):
+	# 	w = Worker(Configuration('test/fixtures/config/local_calendar.cfg'))
+	# 	w.execute()
+	# 	assert False
+
+
+class TestFetcher(object):
+
+	def test_local_fetcher(self):
+		f = LocalCalendarEventFetcher()
+		events = list(f.fetch(CalendarConfig(name='test', url='tests/fixtures/calendars/single_event.ics')))
+		assert len(events) == 1
+
+		event = events[0]
+
+		assert event.name == 'Test Event'
+		assert event.start == datetime.datetime(2015, 12, 20, 9, 0, tzinfo=pytz.UTC)
+		assert event.end == datetime.datetime(2015, 12, 20, 10, 0, tzinfo=pytz.UTC)
+
+
+class TestFetcherUtils(object):
+
+	def test_dt_conversion_none_arg(self):
+		return _to_utc_datetime(None) is None
+
+	def test_dt_conversion_already_utc(self):
+		dt = datetime.datetime(2015, 12, 20, tzinfo=pytz.UTC)
+		assert _to_utc_datetime(dt) == dt
+
+	def test_dt_conversion_different_timezones(self):
+		dt = datetime.datetime(2015, 12, 20, 10, 0, 0, tzinfo=pytz.timezone('Europe/Berlin'))
+		assert _to_utc_datetime(dt) == datetime.datetime(2015, 12, 20, 9, 0, 0, tzinfo=pytz.UTC)
