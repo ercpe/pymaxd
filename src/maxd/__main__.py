0b19fa4dbd9c7bfc727299068908792f0e50cc86
Johann Schmitz <johann@j-schmitz.net>
2015-12-25 17:12:38 +0100

    Allow enabling debug message from pymax library

--- src/maxd/__main__.py
+++ src/maxd/__main__.py
@@ -11,7 +11,8 @@ from maxd.daemon import Daemon
 if __name__ == "__main__":  # pragma: nocover
 	parser = ArgumentParser()
 	parser.add_argument('-c', '--config', default='/etc/maxd.cfg', help="Config file to use (default: %(default)s)")
-	parser.add_argument('-v', '--verbose', action="count", default=1)
+	parser.add_argument('-v', '--verbose', action="count", default=1, help='Increase verbosity')
+	parser.add_argument('-d', '--debug', action='store_true', default=False, help="Enabled debug messages from the pymax library")
 
 	parser.add_argument('--log-target', default='syslog')
 
@@ -22,6 +23,10 @@ if __name__ == "__main__":  # pragma: nocover
 							handlers=(SysLogHandler('/dev/log', facility=SysLogHandler.LOG_DAEMON), ))
 	else:
 		logging.basicConfig(level=logging.FATAL - (10 * args.verbose), format='%(asctime)s %(levelname)-7s %(message)s')
+
+	if not args.debug:
+		pymax_logger = logging.getLogger('pymax')
+		pymax_logger.setLevel(logging.WARNING)
 
 	daemon = None
 
